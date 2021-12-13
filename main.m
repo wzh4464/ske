2755efca1f6bec9a7fc52690c15e7c41906548ca
zihanwu7 <zihanwu7@CY5BN33.mshome.net>
2021-12-13 10:06:02 +0800

    arcthin syn to m

--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-load ('hockeyarcpoints.txt','-ascii',arcpoints)
+arcpoints = load ('hockeyarcpoints.txt','-ascii');
 [sizex,~] = size(arcpoints);
 
 arc = 0;
@@ -9,15 +9,26 @@ for i = 1 : sizex
     arc_label = arcpoints(i,1);
     if arc~=arc_label
         if arc > 0
-            cellArc{arc} = F;
+            cellArc{new_label} = F;
         end
         arc = arc_label;
         new_label = new_label+1;
         F = [];
         k = 1;
     end
-    F(k,1) = arcpoints(i,2);
-    F(k,2) = arcpoints(i,3);
+    F(k,1) = arcpoints(i,2)+1;
+    F(k,2) = arcpoints(i,3)+1;
     F(k,3) = arcpoints(i,1);
     k = k + 1;
 end
+% Successfully saved arcs to cellArc
+% coordinate +1, +1
+
+[~, arc_num] = size(cellArc);
+thinArc = cell(arc_num,1)
+for i = 1 : arc_num
+    [point_num,~] = size(cellArc{i});
+    img = full(sparse(cellArc{i}(:,1),cellArc{i}(:,2),ones(point_num,1)));
+    [thinArc{i}(:,1),thinArc{i}(:,2),thinArc{i}(:,3)] = find(bwskel(logical(img)));
+end
+% thin Arc Done
