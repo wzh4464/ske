b16ec5903a5f81f726667955a064cfbf57fbac18
wzh4464 <wzh4464@gmail.com>
2022-07-05 11:09:38 +0800

    7.5

--- new.m
+++ new.m
@@ -1,52 +1,93 @@
-clc;
-% clear all;
-% load("/home/wu/codes/cocluster_matlab/out_ellipse_666.txt");
-load("/home/wu/codes/ELSDc/out_ellipse.txt");
-% tmp = out_ellipse(:,6:12);
-% output = [out_ellipse(:,6:10),abs(out_ellipse(:,12)-out_ellipse(:,11))];
-output = out_ellipse(:,6:10);
-% drawEllipses(output',"666.jpg",0)
-% ind = cocluster()
-out_num = size(output,1);
-cor = zeros(out_num);
-for i = 1 : out_num
-    for j = 1 : out_num
-        cor(i,j) = Correlation(output(i,:),output(j,:));
-    end
-end
-[u,s,~]=svd(cor);
-R = u*s^(1/2);
-sums = NaN(1,50);
-for j = 2:10 % kmeans 的 k
-    [ind,~,sumd] = kmeans(R,j);
-    [~,I]=sort(ind);
-    newcor = cor(I,I);
-    img=mat2gray(newcor);%将数值矩阵X转换为灰度图像
-    %     C(j)={M};
-    %     id(j)={ind};
-    sums(j) = sum(sumd);
-    disp(sums(j))
-    disp(j)
-    imwrite(img,"data/chess"+string(j)+'.jpg')
-    tmp = zeros(j,5);
-    cnt = zeros(j);
-%     % average: very wrong
+% @Author: WU Zihan
+% @Date:   2022-06-26 14:42:14
+% @Last Modified by:   WU Zihan
+% @Last Modified time: 2022-07-04 20:43:01
+%% loading
+% clc;
+% % clear all;
+% % load("/home/wu/codes/cocluster_matlab/out_ellipse_666.txt");
+% load("/home/wu/codes/ELSDc/out_ellipse.txt");
+% % tmp = out_ellipse(:,6:12);
+% % output = [out_ellipse(:,6:10),abs(out_ellipse(:,12)-out_ellipse(:,11))];
+% output = out_ellipse(:,6:10);
+% % drawEllipses(output',"666.jpg",0)
+% % ind = cocluster()
+% out_num = size(output,1);
+% cor = zeros(out_num);
+% for i = 1 : out_num
+%     for j = 1 : out_num
+%         cor(i,j) = Correlation(output(i,:),output(j,:));
+%     end
+% end
+
+%% R decomposition 
+% [u,s,~]=svd(cor);
+% R = u*s^(1/2);
+% sums = NaN(1,50);
+% for j = 2:10 % kmeans 的 k
+%     [ind,~,sumd] = kmeans(R,j);
+%     [~,I]=sort(ind);
+%     newcor = cor(I,I);
+%     img=mat2gray(newcor);%将数值矩阵X转换为灰度图像
+%     %     C(j)={M};
+%     %     id(j)={ind};
+%     sums(j) = sum(sumd);
+%     disp(sums(j))
+%     disp(j)
+%     imwrite(img,"data/chess"+string(j)+'.jpg')
+%     tmp = zeros(j,5);
+%     cnt = zeros(j);
+% %     % average: very wrong
+% %     for i = 1:size(output,1)
+% %         tmp(ind(i),:) = tmp(ind(i),:)+output(i,:);
+% %         cnt(ind(i)) = cnt(ind(i)) + 1;
+% %     end
+% %     for i = 1:j
+% %         tmp(i,:) = tmp(i,:)/cnt(i);
+% %     end
 %     for i = 1:size(output,1)
-%         tmp(ind(i),:) = tmp(ind(i),:)+output(i,:);
-%         cnt(ind(i)) = cnt(ind(i)) + 1;
+%         tmp(ind(i),:) = output(i,:);
 %     end
-%     for i = 1:j
-%         tmp(i,:) = tmp(i,:)/cnt(i);
+%     drawEllipses(tmp',"666.jpg",j)
+% end
+% plot(sums)
+
+%% Yan's own word
+
+% [u,s,~]=svd(cor);
+% R = (u*s);
+% % R=R(1:3,:);
+% sums = NaN(1,50);
+% for j = 2:10 % kmeans 的 k
+%     [ind,~,sumd] = kmeans(R,j);
+%     [~,I]=sort(ind);
+%     newcor = cor(I,I);
+%     img=mat2gray(newcor);%将数值矩阵X转换为灰度图像
+%     %     C(j)={M};
+%     %     id(j)={ind};
+%     sums(j) = sum(sumd);
+% %     disp(sums(j))
+% %     disp(j)
+% %     imwrite(img,"data/chess"+string(j)+'.jpg')
+%     tmp = zeros(j,5);
+% %     cnt = zeros(j);
+% %     % average: very wrong
+% %     for i = 1:size(output,1)
+% %         tmp(ind(i),:) = tmp(ind(i),:)+output(i,:);
+% %         cnt(ind(i)) = cnt(ind(i)) + 1;
+% %     end
+% %     for i = 1:j
+% %         tmp(i,:) = tmp(i,:)/cnt(i);
+% %     end
+%     for i = 1:size(output,1)
+%         tmp(ind(i),:) = output(i,:);
 %     end
-    for i = 1:size(output,1)
-        tmp(ind(i),:) = output(i,:);
-    end
-    drawEllipses(tmp',"666.jpg",j)
-end
-plot(sums)
+%     drawEllipses(tmp',"666.jpg",j)
+% end
+% plot(sums)
 
 %%
-j = 4;
+j = 7;
 [ind,~,sumd] = kmeans(R,j);
 [~,I]=sort(ind);
 newcor = cor(I,I);
--- testing.m
+++ testing.m
@@ -1,41 +1,78 @@
-n = 19;
-a = out_ellipse(n, 8);
-b = out_ellipse(n, 9);
-x0 = out_ellipse(n, 6);
-y0 = out_ellipse(n, 7);
-theta = out_ellipse(n, 10);
-phi1 = out_ellipse(n, 11);
-phi2 = out_ellipse(n, 12);
-x1 = out_ellipse(n, 2);
-y1 = out_ellipse(n, 3);
-x2 = out_ellipse(n, 4);
-y2 = out_ellipse(n, 5);
+% n = 27;
+% a = out_ellipse(n, 8);
+% b = out_ellipse(n, 9);
+% x0 = out_ellipse(n, 6);
+% y0 = out_ellipse(n, 7);
+% theta = out_ellipse(n, 10);
+% phi1 = out_ellipse(n, 11);
+% phi2 = out_ellipse(n, 12);
+% x1 = out_ellipse(n, 2);
+% y1 = out_ellipse(n, 3);
+% x2 = out_ellipse(n, 4);
+% y2 = out_ellipse(n, 5);
+% % 
+% % rosin_dist([x1,y1],[x0,y0,a,b,theta])
+% astart = atan2((y1-y0),(x1-x0));
+% aend = atan2((y2-y0),(x2-x0));
+% 
+% if astart < 0
+%     astart = astart + 2 * pi;
+% end
+% if aend < 0
+%     aend = aend + 2 * pi;
+% end
+% 
+% if astart > aend 
+%     tang = aend + 2 * pi;
+% else
+%     tang = aend;
+% end
+% astart
+% aend
+% astart-phi1
+% aend-phi2
+% 
+% % x0 + a * cos(theta) * cos (phi1) + b * sin(theta) * sin(phi1)
+% 
+% set = arcpoints(out_ellipse(n,:));%arc points
+% 
+% figure;
+% scatter(set(:,1),set(:,2));
+% axis equal;
 
-rosin_dist([x1,y1],[x0,y0,a,b,theta])
-% atan2((y1-y0),(x1-x0)) + 2 * pi
-% atan2((y2-y0),(x2-x0)) + 2 * pi
-% x0 + a * cos(theta) * cos (phi1) + b * sin(theta) * sin(phi1)
+% for i = 1:size(out_ellipse,1)
+%     point{i} = arcpoints(out_ellipse(i,:));
+% end
+% 
+% for i = 1:size(out_ellipse,1)
+%     for phi = 0:0.5:360
+%         phi_ = phi /180 * pi;
+%         x = x0 + a * cos(theta) * cos (phi_) - b * sin(theta) * sin(phi_);
+%         y = y0 + a * cos(theta) * sin (phi_) + b * sin(theta) * cos(phi_);
+%         set = [set;[round(x),round(y)]];
+%     end
+%     ellipse_point{i} = ellipsePoint(out_ellipse(i,6:10));
+% end
+% 
+% 
+% %
+% DM = distanceMatrix(out_ellipse(:,:),point,ellipse_point,arc_len)
+% 
+% group=[1,3,7,8,19,23];
+% dm = DM(group,group)
+% %%
+% DM
 
-set = arcpoints(out_ellipse(9,:));%arc points
-for i = 1:size(out_ellipse,1)
-    point{i} = arcpoints(out_ellipse(i,:));
+for n=1:27
+    x0 = out_ellipse(n, 6);
+    y0 = out_ellipse(n, 7);
+    x1 = out_ellipse(n, 2);
+    y1 = out_ellipse(n, 3);
+    x2 = out_ellipse(n, 4);
+    y2 = out_ellipse(n, 5);
+    out_ellipse(n,14) = atan2((y1-y0),(x1-x0));
+    out_ellipse(n,15) = atan2((y2-y0),(x2-x0));
 end
 
-for i = 1:size(out_ellipse,1)
-    for phi = 0:0.5:360
-        phi_ = phi /180 * pi;
-        x = x0 + a * cos(theta) * cos (phi_) - b * sin(theta) * sin(phi_);
-        y = y0 + a * cos(theta) * sin (phi_) + b * sin(theta) * cos(phi_);
-        set = [set;[round(x),round(y)]];
-    end
-    ellipse_point{i} = ellipsePoint(out_ellipse(i,6:10));
-end
-
-
-%
-DM = distanceMatrix(out_ellipse(:,:),point,ellipse_point,arc_len)
-
-group=[1,3,7,8,19,23];
-dm = DM(group,group)
-%%
-DM
+diff = out_ellipse(:,14:15)-out_ellipse(:,11:12);
+    
