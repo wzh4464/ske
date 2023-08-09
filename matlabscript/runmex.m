05a619d34b86a394ce846bab9e402922e5e95576
Hance Wu <32484940+wzh4464@users.noreply.github.com>
2023-08-09 15:32:16 +0800

    mex error

--- matlabscript/runmex.m
+++ matlabscript/runmex.m
@@ -1,9 +1,16 @@
+%% mex and run
 try
     mexall
-    [out, pgm] = mexELSDc('../toPGM/666.pgm');
+    [out,label, pgm] = mexELSDc('../toPGM/666.pgm');
     pgmmax = max(max(pgm));
-    pgm = double(pgm)/double(pgmmax);
+    pgmshow = double(pgm)/double(pgmmax);
     save('../toPGM/pgm666.mat', 'pgm');
+    imshow(pgmshow);
 catch
     disp('mexELSDc failed')
 end
+
+%% test
+
+% count different numbers in pgm
+num = unique(pgm);
